e299d7c96a404f3a4128a489d6ad4322dee0cdb6
Viihohn02 <95841706+Viihohn02@users.noreply.github.com>
2021-12-09 03:30:51 -0300

    Initialize contributing-license-link

---
+++ enable-automated-security-fixes-for-org.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+
+require('dotenv').config()
+
+const Octokit = require('@octokit/rest')
+const octokit = new Octokit({
+  auth: process.env.GH_AUTH_TOKEN,
+  previews: ['london-preview']
+})
+
+const [, , ...args] = process.argv
+const owner = args
+const options = octokit.repos.listForOrg.endpoint.merge({org: owner, type: 'all'})
+
+octokit
+  .paginate(options)
+  .then(repositories => {
+    for (const repository of repositories) {
+      if (!repository.archived) {
+        const repo = repository.name
+
+        octokit.repos
+          .enableAutomatedSecurityFixes({
+            owner,
+            repo
+          })
+          .then(response => {
+            if (response && response.status === 204) {
+              console.log(`Success for ${owner}/${repo}`)
+            } else {
+              console.log(`Failed for ${owner}/${repo}`)
+            }
+          })
+          .catch(error => {
+            console.error(`Failed for ${owner}/${repo}
+  ${error.message}
+  ${error.documentation_url}
+`)
+          })
+      }
+    }
+  })
+  .catch(error => {
+    console.error(`Getting repositories for organization ${owner} failed.
+  ${error.message} (${error.status})
+  ${error.documentation_url}
+`)
+  })
